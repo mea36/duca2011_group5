f3ce8872ca89f70000b76d48935051f8b076125d
Danny Glaser-Reich <dglaser-reich@ducomputingacademy.org>
2011-07-22 15:19:52 -0400

    for alex third try it is for the add button

---
+++ ClassMate/Classes/DescriptionView.h
@@ -0,0 +1,16 @@
+//
+//  DescriptionView.h
+//  ClassMate
+//
+//  Created by CS Admin on 7/22/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface DescriptionView : UIViewController {
+
+}
+
+@end
