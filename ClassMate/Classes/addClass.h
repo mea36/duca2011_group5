e139f0283d25bc11fe8a93c8cf10ded1a0d49f41
Tom Harrison <tharrison@ducomputingacademy.org>
2011-07-26 15:20:24 -0400

    Committing files

---
+++ ClassMate/Classes/addClass.h
@@ -0,0 +1,25 @@
+//
+//  addClass.h
+//  ClassMate
+//
+//  Created by CS Admin on 7/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "EgoDb.h"
+#import "ClassObj.h"
+
+
+
+@interface addClass : UIViewController <UIPickerViewDataSource, UIPickerViewDelegate> {
+	
+	IBOutlet UIPickerView *pickerView;
+	NSArray *courses;
+
+}
+
+@property (nonatomic, retain) IBOutlet UIPickerView *pickerView;
+@property (nonatomic, retain) NSArray *courses;
+
+@end
---
+++ ClassMate/Classes/addClass.m
@@ -0,0 +1,83 @@
+//
+//  addClass.m
+//  ClassMate
+//
+//  Created by CS Admin on 7/25/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "addClass.h"
+#import "EgoDb.h"
+
+
+@implementation addClass
+
+@synthesize courses; 
+@synthesize pickerView;
+
+// The designated initializer.  Override if you create the controller programmatically and want to perform customization that is not appropriate for viewDidLoad.
+/*
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization.
+    }
+    return self;
+}
+*/
+
+
+// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
+- (void)viewDidLoad {
+    [super viewDidLoad];
+	self.courses = [[EgoDb database] getListofCourses];
+}
+
+
+/*
+// Override to allow orientations other than the default portrait orientation.
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation {
+    // Return YES for supported orientations.
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+*/
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+	
+	return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+	
+	return [self.courses count];
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+	return [self.courses objectAtIndex:row];
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+	
+}
+
+
+
+- (void)didReceiveMemoryWarning {
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc. that aren't in use.
+}
+
+- (void)viewDidUnload {
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
